2edc6eaa65ca3e9508de5b6421b278420b9adcec
stormofcodes <stormofcodes@gmail.com>
2025-05-02 12:30:13 +0200

    Add operator function

--- script.js
+++ script.js
@@ -14,5 +14,17 @@ const divide = function (a, b) {
     return a / b;
 }
 
+const operate = function(operator, a, b)  {
+switch (operator) {
+    case '+':
+        return add(a, b);
+    case '-':
+        return subtract(a, b);
+    case '*':
+        return multiply(a, b);
+    case '/':
+        return divide(a, b);
+}
+};
 
-console.log();
+
